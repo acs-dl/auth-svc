578b6379b121f642497de8b4aa473d2693bb19c4
Maksym Hrynenko <mg06062003@gmail.com>
2023-02-17 15:27:33 +0200

    add print

--- middlewares/check_host.go
+++ middlewares/check_host.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"gitlab.com/distributed_lab/ape"
 	"gitlab.com/distributed_lab/ape/problems"
 	"net/http"
@@ -11,7 +12,7 @@ func CheckHost(programHost string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			requestHost := r.Host
-
+			fmt.Println(requestHost)
 			if requestHost == programHost {
 				next.ServeHTTP(w, r)
 				return
